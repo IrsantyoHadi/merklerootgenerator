0fc830107ae587a3059fcbafdff10be408272eff
irsantyo <irsantyo@vcgamers.com>
2022-05-11 02:02:22 +0700

    update for kuro before testing

--- merkle.js
+++ merkle.js
@@ -1,23 +1,19 @@
 const keccak256 = require('keccak256');
 const { MerkleTree } = require('merkletreejs');
 
-const whitelistAddresses = [
-    "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4",
-    "0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2",
-    "0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db"
-]
+const whitelistAddresses = ['0x44f3e2af32Be676689e26C2eB2F3f19fEa91e1BD', '0x9E3EF64ebf7c4E3146f42B40a21E7D600B5A911b'];
 
-const leafNodes = whitelistAddresses.map(el => keccak256(el));
+const leafNodes = whitelistAddresses.map((el) => keccak256(el));
 
-const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs : true})
+const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true });
 
 const rootHash = merkleTree.getHexRoot();
 
-console.log('MERKLE ROOT: ',rootHash); // merkle root 
+console.log('MERKLE ROOT: ', rootHash); // merkle root
 
-const claimingAddress = leafNodes[0]
+const claimingAddress =
+  leafNodes[whitelistAddresses.findIndex((el) => el === '0x44f3e2af32Be676689e26C2eB2F3f19fEa91e1BD')];
 
 const hexProof = merkleTree.getHexProof(claimingAddress);
 
-
-console.log('HEX PROOF:',hexProof);
+console.log('HEX PROOF:', hexProof);
